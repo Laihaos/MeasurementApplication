7c9a935f1e9ac34c32b1106e7a8e2269b561e14a
mishi <773580832@qq.com>
2024-01-02 19:02:39 +0800

    命盘1

--- app/src/main/java/com/example/measurementapplication/Natal_chart.java
+++ app/src/main/java/com/example/measurementapplication/Natal_chart.java
@@ -21,6 +21,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Natal_chart extends AppCompatActivity {
 
@@ -43,14 +44,15 @@ public class Natal_chart extends AppCompatActivity {
     public static String birthday;
     static String[] HeavenlyStems = {"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"};
     static String[] EarthlyBranches = {"子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"};
+    static String[] ming = {"命宫", "父母", "福德", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"};
+
     static String[] ziweiBranches = {"寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥", "子", "丑"};
 
     static String[] birthdayBranches = {"寅", "丑", "子", "亥", "戌", "酉", "申", "未", "午", "巳", "辰", "卯"};
 
-    static String[] zuofu = {"辰", "巳", "午", "未", "申", "酉", "戌", "亥", "子", "丑","寅", "卯"};
+    static String[] zuofu = {"辰", "巳", "午", "未", "申", "酉", "戌", "亥", "子", "丑", "寅", "卯"};
 
-    static String[] youbi = { "亥", "子", "丑","寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌"};
-
+    static String[] youbi = {"亥", "子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌"};
 
 
     static String wuxing = "";
@@ -58,10 +60,12 @@ public class Natal_chart extends AppCompatActivity {
     static int wuXingJu = 0;
     static int birthDay = 0;
 
-    static String ziwei ;
+    static String ziwei;
     static ArrayList<String> HeavenlyStemslist = new ArrayList<>();
     static ArrayList<String> EarthlyBrancheslist = new ArrayList<>();
     static ArrayList<String> ziweiBrancheslist = new ArrayList<>(Arrays.asList(ziweiBranches));
+    static ArrayList<String> wenqulist = new ArrayList<>(Arrays.asList(zuofu));
+
     static ArrayList<String> XingyaoBrancheslist = new ArrayList<>();
     static Map<String, String> sihuamap = new HashMap<>();
 
@@ -101,7 +105,7 @@ public class Natal_chart extends AppCompatActivity {
         return position;
     }
 
-    public static void Xingyao(int ziWeiStarPosition, List<String> ziweiBrancheslist, List<String> XingyaoBrancheslist, String ziwei,List<TextView> Textlist,String xingyao) {
+    public static void Xingyao(int ziWeiStarPosition, List<String> ziweiBrancheslist, List<String> XingyaoBrancheslist, String ziwei, List<TextView> Textlist, String xingyao) {
 
 
         for (int i = 0; i < ziweiBrancheslist.size(); i++) {
@@ -111,14 +115,13 @@ public class Natal_chart extends AppCompatActivity {
                 if (((i - ziWeiStarPosition) % 12) >= 0) {
 
                     XingyaoBrancheslist.add(ziweiBrancheslist.get((i - ziWeiStarPosition) % 12));
-                    SpannableString newSpannableString = new SpannableString(xingyao+"\n"+Textlist.get((i - ziWeiStarPosition) % 12).getText());
+                    SpannableString newSpannableString = new SpannableString(xingyao + "\n" + Textlist.get((i - ziWeiStarPosition) % 12).getText());
                     newSpannableString.setSpan(new ForegroundColorSpan(Color.RED), 0, newSpannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                     Textlist.get((i - ziWeiStarPosition) % 12).setText(newSpannableString);
 
-
-                } else  {
+                } else {
                     XingyaoBrancheslist.add(ziweiBrancheslist.get(((i - ziWeiStarPosition) % 12) + 12));
-                    SpannableString newSpannableString = new SpannableString(xingyao+"\n"+Textlist.get(((i - ziWeiStarPosition) % 12) + 12).getText());
+                    SpannableString newSpannableString = new SpannableString(xingyao + "\n" + Textlist.get(((i - ziWeiStarPosition) % 12) + 12).getText());
                     newSpannableString.setSpan(new ForegroundColorSpan(Color.RED), 0, newSpannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                     Textlist.get(((i - ziWeiStarPosition) % 12) + 12).setText(newSpannableString);
 
@@ -139,23 +142,21 @@ public class Natal_chart extends AppCompatActivity {
         context.startActivity(intent);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void dateActivity() {
         setContentView(R.layout.acyivity_natal_chart);
 
-        textyan =  findViewById(R.id.yan);
-        textmou =  findViewById(R.id.mou);
-        textchen =  findViewById(R.id.chen);
-        textji =  findViewById(R.id.ji);
-        textwu =  findViewById(R.id.wu);
-        textwei =  findViewById(R.id.wei);
-        textshen =  findViewById(R.id.shen);
-        textxi =  findViewById(R.id.xi);
-        textxu =  findViewById(R.id.xu);
-        texthai =  findViewById(R.id.hai);
-        textzi =  findViewById(R.id.zi);
-        textchou =  findViewById(R.id.chou);
+        textyan = findViewById(R.id.yan);
+        textmou = findViewById(R.id.mou);
+        textchen = findViewById(R.id.chen);
+        textji = findViewById(R.id.ji);
+        textwu = findViewById(R.id.wu);
+        textwei = findViewById(R.id.wei);
+        textshen = findViewById(R.id.shen);
+        textxi = findViewById(R.id.xi);
+        textxu = findViewById(R.id.xu);
+        texthai = findViewById(R.id.hai);
+        textzi = findViewById(R.id.zi);
+        textchou = findViewById(R.id.chou);
         TextViewlist.add(textyan);
         TextViewlist.add(textmou);
         TextViewlist.add(textchen);
@@ -170,7 +171,7 @@ public class Natal_chart extends AppCompatActivity {
         TextViewlist.add(textchou);
 
         for (int i = 0; i < TextViewlist.size(); i++) {
-            TextView a =TextViewlist.get(i);
+            TextView a = TextViewlist.get(i);
             a.setText("");
         }
 
@@ -213,6 +214,7 @@ public class Natal_chart extends AppCompatActivity {
             if (i == birthdaylist.indexOf(birthday)) {
 
 //                System.out.println(EarthlyBrancheslist);
+                Log.d("EarthlyBrancheslist: ", String.valueOf(EarthlyBrancheslist));
                 break;
             }
 
@@ -223,6 +225,10 @@ public class Natal_chart extends AppCompatActivity {
             if (i == Month - 1) {
 //              命宫
                 System.out.println(EarthlyBrancheslist.get(i));
+                Log.d("命宫: ", String.valueOf(EarthlyBrancheslist));
+
+                Log.d("命宫: ", String.valueOf(EarthlyBrancheslist.indexOf(EarthlyBrancheslist.get(i))));
+
 //                textyan.append("\n"+EarthlyBrancheslist.get(i));
                 for (int j = 0; j < EarthlyBranches.length; j++) {
 
@@ -351,15 +357,20 @@ public class Natal_chart extends AppCompatActivity {
         }
         System.out.println(wuxing);
 
+
         int ziWeiStarPosition = ZiWeiStarPosition(birthDay, wuXingJu);
+
+        System.out.println("ziWeiStarPosition：" + ziWeiStarPosition);
+
 
         for (int i = 0; i < ziweiBrancheslist.size(); i++) {
 
-            if (i == ziWeiStarPosition) {
+            if (i == (ziWeiStarPosition - 1)) {
 
-                System.out.println("紫微星的位置在：" + ziweiBrancheslist.get(i - 1) + "宫");
+
+                System.out.println("紫微星的位置在：" + ziweiBrancheslist.get(i) + "宫");
 //                紫薇
-                ziwei=ziweiBrancheslist.get(i - 1);
+                ziwei = ziweiBrancheslist.get(i);
 
                 Log.d("ziweiBrancheslist", String.valueOf(ziweiBrancheslist.indexOf(ziwei)));
 
@@ -367,111 +378,120 @@ public class Natal_chart extends AppCompatActivity {
 
         }
 
-        Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"紫薇");
+        Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "紫薇");
 
 //        天机
-        Xingyao(1, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天机");
+        Xingyao(1, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天机");
 
 //        太阳
-        Xingyao(3, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"太阳");
+        Xingyao(3, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "太阳");
 
 //        武曲
-        Xingyao(4, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"武曲");
+        Xingyao(4, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "武曲");
 
 //        天同
-        Xingyao(5, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天同");
+        Xingyao(5, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天同");
 
 //        廉贞
-        Xingyao(8, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"廉贞");
+        Xingyao(8, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "廉贞");
 
         switch (ziweiBrancheslist.indexOf(ziwei)) {
 
             case 0:
-                Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天府");
+                Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天府");
                 break;
             case 6:
-                Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天府");
+                Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天府");
                 break;
             case 1:
-                Xingyao(2, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天府");
+                Xingyao(2, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天府");
 
                 break;
             case 2:
-                Xingyao(4, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天府");
+                Xingyao(4, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天府");
 
                 break;
             case 3:
-                Xingyao(6, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天府");
+                Xingyao(6, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天府");
 
                 break;
             case 4:
-                Xingyao(8, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天府");
+                Xingyao(8, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天府");
 
                 break;
             case 5:
-                Xingyao(10, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天府");
+                Xingyao(10, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天府");
 
                 break;
             case 7:
-                Xingyao(2, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天府");
+                Xingyao(2, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天府");
 
                 break;
             case 8:
-                Xingyao(4, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天府");
+                Xingyao(4, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天府");
 
                 break;
             case 9:
-                Xingyao(6, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天府");
+                Xingyao(6, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天府");
 
                 break;
             case 10:
-                Xingyao(8, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天府");
+                Xingyao(8, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天府");
 
                 break;
             case 11:
-                Xingyao(10, ziweiBrancheslist, XingyaoBrancheslist, ziwei,TextViewlist,"天府");
+                Xingyao(10, ziweiBrancheslist, XingyaoBrancheslist, ziwei, TextViewlist, "天府");
 
                 break;
 
         }
-        Xingyao(-1, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6),TextViewlist,"太阴");
-        Xingyao(-2, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6),TextViewlist,"贪狼");
-        Xingyao(-3, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6),TextViewlist,"巨门");
-        Xingyao(-4, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6),TextViewlist,"天相");
-        Xingyao(-5, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6),TextViewlist,"天梁");
-        Xingyao(-6, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6),TextViewlist,"七杀");
-        Xingyao(-10, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6),TextViewlist,"破军");
+        Xingyao(-1, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6), TextViewlist, "太阴");
+        Xingyao(-2, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6), TextViewlist, "贪狼");
+        Xingyao(-3, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6), TextViewlist, "巨门");
+        Xingyao(-4, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6), TextViewlist, "天相");
+        Xingyao(-5, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6), TextViewlist, "天梁");
+        Xingyao(-6, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6), TextViewlist, "七杀");
+        Xingyao(-10, ziweiBrancheslist, XingyaoBrancheslist, XingyaoBrancheslist.get(6), TextViewlist, "破军");
 
 //        System.out.println("十四主星" + XingyaoBrancheslist);
         List<String> list = Arrays.asList(youbi);
-        Collections.reverse(list);
+        List<StringBuilder> stringBuilderList =
+                list.stream()
+                        .map(StringBuilder::new)
+                        .collect(Collectors.toList());
+        Collections.reverse(stringBuilderList);
+        List<String> reversedList = stringBuilderList.stream()
+                .map(StringBuilder::toString)
+                .collect(Collectors.toList());
+
 
         for (int i = 0; i < zuofu.length; i++) {
-            if (i == Month) {
-                Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, zuofu[i-1],TextViewlist,"左辅");
+            if (i + 1 == Month) {
+                Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, zuofu[i], TextViewlist, "左辅");
             }
-            if (i == Month) {
-                Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, list.get(i-1),TextViewlist,"右弼");
+            if (i + 1 == Month) {
+                Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, reversedList.get(i), TextViewlist, "右弼");
             }
         }
 
 
         for (int i = 0; i < EarthlyBranches.length; i++) {
-            if (birthday.equals(list.get(i))) {
-                Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, EarthlyBranches[i],TextViewlist,"文昌");
+            if (birthday.equals(EarthlyBranches[i])) {
+
+                Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, reversedList.get(i), TextViewlist, "文昌");
             }
-            if (birthday.equals(zuofu[i])) {
-                Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, EarthlyBranches[i],TextViewlist,"文曲");
+            if (birthday.equals(EarthlyBranches[i])) {
+
+                Xingyao(0, ziweiBrancheslist, XingyaoBrancheslist, zuofu[i], TextViewlist, "文曲");
             }
 
         }
 
 
-
 //        ziweiBranches  {"寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥","子", "丑"};
 //    四化
 
-        sihuamap.put("甲","；廉贞破军武曲太阳");
+        sihuamap.put("甲", "廉贞破军武曲太阳");
         sihuamap.put("乙", "天机天梁紫薇太阴");
         sihuamap.put("丙", "天同天机文昌廉贞");
         sihuamap.put("丁", "太阴天同天机巨门");
@@ -483,22 +503,49 @@ public class Natal_chart extends AppCompatActivity {
         sihuamap.put("癸", "破军巨门太阴贪狼");
         String si;
         System.out.println("四化 :" + sihuamap.get(tian));
+        SpannableString spannableString = new SpannableString("要添加样式的文本");
+        spannableString.setSpan(new ForegroundColorSpan(Color.RED), 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
 
         for (int i = 0; i < 4; i++) {
-            si= sihuamap.get(tian).substring(i,i+1);
-            for (int j = 0; j < TextViewlist.size(); j++) {
-                TextView a =TextViewlist.get(j);
+            si = sihuamap.get(tian).substring(i * 2, i * 2 + 2);
+            Log.d("sihuamap: ", si);
 
-//                if (a.getText().equals(si)||si.equals()) {
-//                    Log.d( "onCreate: ", String.valueOf(a));
-//                    a.append("\n"+);
-//                }
+            for (int j = 0; j < TextViewlist.size(); j++) {
+                TextView a = TextViewlist.get(j);
+                String hua = a.getText().toString().replaceAll("(?m)^\\s*\n", "");
+
+                if (hua.contains(si) && i == 0) {
+                    a.append("       禄");
+                } else if (hua.contains(si) && i == 1) {
+                    a.append("       权");
+                } else if (hua.contains(si) && i == 2) {
+                    a.append("       科");
+                } else if (hua.contains(si) && i == 3) {
+                    a.append("       忌");
+                }
+
             }
 
         }
 
-//
-
 
     }
+
+    @Override
+    protected void onRestart() {
+
+        super.onRestart();
+        dateActivity();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        dateActivity();
+
+    }
+
+
 }
